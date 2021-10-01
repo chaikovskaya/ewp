7b3ef9b9a314f65b259bdbaf6cc413c11b69dded
chaikovskaya <post-123@yandex.ru>
2021-10-01 17:33:46 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -200,6 +200,50 @@ function initSliderClients() {
     });
 }
 
+function initSliderBlog() {
+    $(".js-slider-blog").each(function(){
+        var $element = $(this),
+            $list = $element.find('.js-slider-list'),
+            $buttons = $element.find('.js-slider-buttons'),
+            $prev = $element.find('.js-slider-prev'),
+            $next = $element.find('.js-slider-next'),
+            $item = $list.find('.js-slider-item');
+
+        var isStart = $item.length > 1 ? true : false;
+
+        $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+            loop: isStart,
+            mouseDrag: isStart,
+            touchDrag: isStart,
+            autoHeight: false,
+            smartSpeed: 500,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 10,
+                },
+                720: {
+                    items: 2,
+                    mouseDrag: true,
+                },
+                992: {
+                    items: 3,
+                },
+            },
+        }));
+        if (!isStart) {
+            $buttons.remove();
+        }
+        $prev.click(function(){
+            $list.trigger("prev.owl.carousel");
+        });
+        $next.click(function(){
+            $list.trigger("next.owl.carousel");
+        });
+    });
+}
+
 function initResizeWindow() {
     var width = $(window).width();
     if (width <= GLOBAL.mobile) {
@@ -227,4 +271,5 @@ $(document).ready(function () {
 
     initSliderAwards();
     initSliderClients();
+    initSliderBlog();
 });
