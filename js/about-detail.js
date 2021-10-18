533f57b2015a8fabb838e42a464f42441329f6f4
chaikovskaya <post-123@yandex.ru>
2021-10-18 10:39:47 +0300

    gsdlo

--- js/about-detail.js
+++ js/about-detail.js
@@ -36,6 +36,49 @@ function initSliderCompany() {
     });
 }
 
+function initSliderReviews() {
+    $(".js-slider-reviews").each(function(){
+        var $element = $(this),
+            $list = $element.find('.js-slider-list'),
+            $buttons = $element.find('.js-slider-buttons'),
+            $prev = $element.find('.js-slider-prev'),
+            $next = $element.find('.js-slider-next'),
+            $item = $list.find('.js-slider-item');
+
+        var isStart = $item.length > 1 ? true : false;
+
+        $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+            loop: isStart,
+            mouseDrag: isStart,
+            touchDrag: isStart,
+            autoHeight: false,
+            smartSpeed: 500,
+            margin: 30,
+            items: 1,
+            responsive: {
+                0: {
+                    margin: 10,
+                },
+                720: {
+                    mouseDrag: true,
+                },
+                992: {
+                },
+            },
+        }));
+        if (!isStart) {
+            $buttons.remove();
+        }
+        $prev.click(function(){
+            $list.trigger("prev.owl.carousel");
+        });
+        $next.click(function(){
+            $list.trigger("next.owl.carousel");
+        });
+    });
+}
+
 $(document).ready(function () {
     initSliderCompany();
+    initSliderReviews();
 });
